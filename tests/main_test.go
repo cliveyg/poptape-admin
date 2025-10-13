3a8b83d37edfed872b26d704d97d6aa8f17bf0f6
cliveyg <junkcat@gmail.com>
2025-10-13 17:51:31 -0300

    Set log level in tests

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"os"
 	"testing"
 
@@ -18,11 +19,6 @@ func TestMain(m *testing.M) {
 		fmt.Println("Could not load .env file:", err)
 	}
 
-	// Print env to confirm
-	fmt.Println("POSTGRES_USERNAME:", os.Getenv("POSTGRES_USERNAME"))
-	fmt.Println("POSTGRES_DBNAME:", os.Getenv("POSTGRES_DBNAME"))
-	fmt.Println("POSTGRES_HOST:", os.Getenv("POSTGRES_HOST"))
-
 	TestApp = &app.App{}
 	TestApp.Log = setupLogger()
 	TestApp.CommandRunner = &app.RealCommandRunner{}
@@ -34,5 +30,6 @@ func TestMain(m *testing.M) {
 	resetDB(nil, TestApp)
 
 	code := m.Run()
+	gin.SetMode(gin.ReleaseMode)
 	os.Exit(code)
 }
