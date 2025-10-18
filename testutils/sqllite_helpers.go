862fb372327a2c5eae61dcf4d0e32279674cbe4b
cliveyg <junkcat@gmail.com>
2025-10-18 01:47:43 -0300

    Add more unit tests

--- testutils/sqllite_helpers.go
+++ testutils/sqllite_helpers.go
@@ -7,15 +7,12 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	_ "modernc.org/sqlite"
 	"net/http/httptest"
 )
 
 // SetupTestAppWithSQLite returns an App and a gorm.DB using in-memory SQLite with all necessary migrations.
 func SetupTestAppWithSQLite() (*app.App, *gorm.DB) {
-	// Use the modernc.org/sqlite driver by ensuring it is imported above.
-	// GORM's sqlite driver will detect and use it if CGO is disabled and mattn/go-sqlite3 is not present.
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared&_driver=modernc"), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
